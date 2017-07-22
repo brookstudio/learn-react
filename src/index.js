62b43355cff9efc62bce014a5af3b6df133ff803
Brookstudio <brook.studio@outlook.com>
2017-07-23 07:27:51 +0800

    Practice on https://facebook.github.io/react/docs/state-and-lifecycle.html

--- src/index.js
+++ src/index.js
@@ -6,51 +6,37 @@ import registerServiceWorker from './registerServiceWorker';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
-function Avator(props){
-    return (
-        <img className="Avatar"
-            src={props.user.avatarUrl}
-            alt={props.user.name}
-        />
-    );
-}
-function UserInfo(props){
-    return (
-        <div className="UserInfo">
-            <Avator user={props.user} />
-            <div className="UserInfo-name">
-                {props.user.name}
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+    componentDidMount(){
+        this.timerID = setInterval(
+            ()=>this.tick(),
+            1000
+        );
+    }
+    componentDidUnmount(){
+        clearInterval(this.timerID);
+    }
+    tick(){
+        this.setState({
+            date: new Date()
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}</h2>
             </div>
-        </div>
-    );
+        );
+    }
 }
-function Comment(props){
-    return (
-        <div className="Comment">
-            <UserInfo user={props.author} />
-            <div className="Comment-text">
-                {props.text}
-            </div>
-            <div className="Comment-data">
-                {formatDate(props.date)}
-            </div>
-        </div>
-    )
-}
-function formatDate(date){
-    return date.toLocaleTimeString();
-}
-const comment = {
-    author:{
-        name:'Tom',
-        avatarUrl:'https://facebook.github.io/react/img/logo.svg'
-    },
-    text:'This is a comment',
-    date:new Date()
-};
 
 ReactDOM.render(
-    <Comment author={comment.author} text={comment.text} date={comment.date} />,
+    <Clock />,
     document.getElementById('root')
 );
 
