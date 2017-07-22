5d563a3ed25133d44675a79d321d0b62459b094b
Brookstudio <brook.studio@outlook.com>
2017-07-23 06:59:27 +0800

    Pratise1 on https://facebook.github.io/react/docs/introducing-jsx.html

--- src/index.js
+++ src/index.js
@@ -6,8 +6,23 @@ import registerServiceWorker from './registerServiceWorker';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-)
+  element,
+  document.getElementById('root')
+);
 registerServiceWorker();
